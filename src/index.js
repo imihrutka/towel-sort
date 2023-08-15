9e95e944dbfb4bd51614547a97d247cce8daf512
imihrutka <dbepka@gmail.com>
2023-08-15 23:23:07 +0300

    feat: implement towelSort function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
-// You should implement your task here.
+
+/**
+ * Sorts a matrix in a "towel" pattern.
+ *
+ * @param {Array} matrix - The matrix to be sorted.
+ * @return {Array} - The sorted matrix in "towel" pattern.
+ */
 
 module.exports = function towelSort (matrix) {
-  return [];
+    
+    if(matrix == undefined) {
+        return [];
+    }
+    const result = matrix.reduce(function (acc, item, index) {
+        if (index % 2) {
+            return acc.concat(item.reverse());
+        } else {
+            return acc.concat(item);
+        }
+    }, []);
+
+    return result;
 }
